d9b745c9bc34308911299b790ad461ce7285f32c
Shahidul-PHP <Shahidul.shovon14@gmail.com>
2022-11-20 18:59:13 +0600

    done

--- logo/logo_post.php
+++ logo/logo_post.php
@@ -12,12 +12,15 @@ $name = $logo['name'];
 
 if(in_array($extension,$allow)){
     if($logo['size'] <= 4000000){
+        // data insert in database
         $insert= "INSERT INTO logos(logo) VALUES('$name')";
         mysqli_query($db_connect,$insert);
+        //make file name
         $last_id = mysqli_insert_id($db_connect);
         $file_name = $last_id . '.' .$extension;
         $new_location = '../uploads/logo/' .$file_name;
         move_uploaded_file($logo['tmp_name'], $new_location);
+        // send update image data in database
         $update =  "UPDATE logos SET logo='$file_name' WHERE id = $last_id";
         mysqli_query($db_connect,$update);
         header('location:add_logo.php');  
